ac60e71a139b925474d62b9c31f264052a4759aa
IrvingShu <810315118@qq.com>
2017-02-28 01:29:17 +0800

    a

---
+++ build/CMakeFiles/test_cuda_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_cuda_project.dir/./test_cuda_project_generated_kernel.cu.o"
+  "CMakeFiles/test_cuda_project.dir/main.cpp.o"
+  "test_cuda_project.pdb"
+  "test_cuda_project"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_cuda_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+extern "C"
+void runCudaPart();
+
+int main(int argc, char *argv[])
+{
+    runCudaPart();
+}
